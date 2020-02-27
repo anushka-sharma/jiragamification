9e6fe665e73d4b12fe365b386f21395c6484fe28
Anushka Sharma <55789857+anushkadevops@users.noreply.github.com>
2020-02-27 16:08:51 +0530

    Update jiraCollector.groovy

--- vars/jiraCollector.groovy
+++ vars/jiraCollector.groovy
@@ -50,5 +50,5 @@ String Key=d.replaceAll("\\[", "").replaceAll("\\]","");*/
 //collecting all issues assigned to a user with username with status "in-progress"
 //  sh "curl -X GET -i -H  -d  -u rig:digitalrig@123 http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=assignee='${emailid/username}'%20AND%20(status%3D"In%20Progress") "
 
-}
-*/
+//}
+

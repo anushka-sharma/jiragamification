582519a274ca9c7027206d435f4601f223f35c5b
Anushka Sharma <55789857+anushkadevops@users.noreply.github.com>
2020-02-27 15:25:22 +0530

    Update jiraCollector.groovy

--- vars/jiraCollector.groovy
+++ vars/jiraCollector.groovy
@@ -1,3 +1,28 @@
+import groovy.json.* 
+  
+def inprogress(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.alm)
+
+String a=jsonObj.alm.projects.project.project_name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+  
+env.name = projectName
+
+  withCredentials([usernamePassword(credentialsId: 'jira_password', passwordVariable: 'password', usernameVariable:'username')]){
+def done = sh """
+     curl -X GET \
+    -H -d -u $username:$password \
+     'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}%20AND%20(status%3D'\'"In%20Progress"\'')' \
+  -H 'cache-control: no-cache' > 'Taskdone.json'
+  """
+   
+  }
+}
+
+/*
 def call(json){
 def jsonString = json
 def jsonObj = readJSON text: jsonString
@@ -25,3 +50,4 @@ println(Key)
 //  sh "curl -X GET -i -H  -d  -u rig:digitalrig@123 http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=assignee='${emailid/username}'%20AND%20(status%3D"In%20Progress") "
 
 }
+*/

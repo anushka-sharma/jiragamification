5044daaf23efeb49b744bad2fc668d0a1c9b6621
Anushka Sharma <55789857+anushkadevops@users.noreply.github.com>
2020-02-27 15:21:54 +0530

    Update jiraCollector.groovy

--- vars/jiraCollector.groovy
+++ vars/jiraCollector.groovy
@@ -15,7 +15,7 @@ println(Key)
 //collecting all issues with status To-do in a project
 // sh "curl -X GET -i -H  -d  -u rig:digitalrig@123 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${Key}%20AND%20(status%3D'\'"To%20Do"\'')'"
 //collecting all issues with status In-Progress in a project
-  sh '''curl -X GET -i -H  -d  -u rig:digitalrig@123 http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${Key}%20AND%20(status%3D'\'"In%20Progress"\'')'''
+  sh '''curl -X GET -i -H  -d  -u rig:digitalrig@123 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${Key}%20AND%20(status%3D'\'"In%20Progress"\'')''''
 println(Key)
  //collecting all issues assigned to a user with username which are in done state
 //  sh "curl -X GET -i -H  -d  -u rig:digitalrig@123 http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=assignee='${emailid/username}'%20AND%20(status%3DDONE) "

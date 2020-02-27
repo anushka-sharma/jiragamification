d42b261afa913caf6de403d1417629ef95d6ca90
Anushka Sharma <55789857+anushkadevops@users.noreply.github.com>
2020-02-27 16:15:40 +0530

    Update jiraCollector.groovy

--- vars/jiraCollector.groovy
+++ vars/jiraCollector.groovy
@@ -49,6 +49,31 @@ sh """
 
 
 
+def todo(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.alm)
+
+String a=jsonObj.alm.projects.project.project_name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+  
+env.name = projectName
+
+  withCredentials([usernamePassword(credentialsId: 'jira_password', passwordVariable: 'password', usernameVariable:'username')]){
+sh """
+     curl -X GET \
+    -H -d -u $username:$password \
+     'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}%20AND%20(status%3D'\'"To%20Do"\'')' \
+  -H 'cache-control: no-cache' 
+  """
+   
+  }
+}
+
+
+
+
 /*
 def call(json){
 def jsonString = json

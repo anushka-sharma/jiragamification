25ae7c42b87c30b30b4ae3a90916212908c603fa
Anushka Sharma <55789857+anushkadevops@users.noreply.github.com>
2020-03-02 16:41:09 +0530

    Update jiraCollector.groovy

--- vars/jiraCollector.groovy
+++ vars/jiraCollector.groovy
@@ -27,7 +27,8 @@ def total = resultJson.total
 echo "Total no.of issues in $projectName with statuts in-progress are $total"
 
 }
-	/*
+
+/*
 
 def jsonSlurper = new JsonSlurper()
 def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/Output.json"))
@@ -51,18 +52,14 @@ for(i=0;i<ecount;i++)
   }
  } 
   
-
-	 def count=JSON.size()
-	 //println(jsonObj.config.emails.email[i])
-	 JCOPY[i]=(JsonOutput.toJson(JSON))
-	 //map.put(ImmutableList.of(jsonObj.config.emails.email[i],count),JCOPY[i])
+def count=JSON.size()
+println(jsonObj.config.emails.email[i])
+JCOPY[i]=(JsonOutput.toJson(JSON))
+map.put(ImmutableList.of(jsonObj.config.emails.email[i],count),JCOPY[i])
 	
 	
-	 JSON.clear()
-	 
-
-	  
-//}
+JSON.clear()
+}
 println(JCOPY)
 */
 
@@ -124,35 +121,3 @@ echo "Total no.of issues in $projectName with statuts to-do are $total"
 	
 }
 
-
-
-
-
-//def call(json){
-//def jsonString = json
-//def jsonObj = readJSON text: jsonString
-
-//String b=jsonObj.alm.projects.project.project_name
-//String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
-//String c=jsonObj.alm.projects.project.project_typeKey 
-//String Key=c.replaceAll("\\[", "").replaceAll("\\]","");
-//String d=jsonObj.alm.projects.project.project_typeKey
-//String Key=d.replaceAll("\\[", "").replaceAll("\\]","");*/
-
-//println(Key)
-
-//collecting all issues with status Done in a project
- //sh "curl -X GET -i -H  -d  -u rig:digitalrig@123 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${Key}%20AND%20(status%3DDONE)'"
-//collecting all issues with status To-do in a project
-// sh "curl -X GET -i -H  -d  -u rig:digitalrig@123 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${Key}%20AND%20(status%3D'\'"To%20Do"\'')'"
-//collecting all issues with status In-Progress in a project
- // sh "curl -X GET -i -H  -d  -u rig:digitalrig@123 'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${Key}%20AND%20(status%3D'\'"In%20Progress"\'')'\
-//println(Key)
- //collecting all issues assigned to a user with username which are in done state
-//  sh "curl -X GET -i -H  -d  -u rig:digitalrig@123 http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=assignee='${emailid/username}'%20AND%20(status%3DDONE) "
-//collecting all issues assigned to a user with username with status "To-do"
-//  sh "curl -X GET -i -H  -d  -u rig:digitalrig@123 http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=assignee='${emailid/username}'%20AND%20(status%3D"To%20Do") "
-//collecting all issues assigned to a user with username with status "in-progress"
-//  sh "curl -X GET -i -H  -d  -u rig:digitalrig@123 http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=assignee='${emailid/username}'%20AND%20(status%3D"In%20Progress") "
-
-//}

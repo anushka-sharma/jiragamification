776b8a9e658a786a6326cf7e4b913b8b4d13db29
Anushka Sharma <55789857+anushkadevops@users.noreply.github.com>
2020-03-02 16:33:07 +0530

    Update jiraCollector.groovy

--- vars/jiraCollector.groovy
+++ vars/jiraCollector.groovy
@@ -83,17 +83,17 @@ sh """
      curl -X GET \
     -H -d -u $username:$password \
      'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}%20AND%20(status%3DDONE)%20order%20by%20duedate&fields=id%2Ckey%2Cpriority' \
-  -H 'cache-control: no-cache' 
+  -H 'cache-control: no-cache' -o outputdone.json
   """
    
   }
-}
+
 
 def jsonSlurper = new JsonSlurper()
 def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/outputdone.json"))
 def total = resultJson.total
 echo "Total no.of issues in $projectName with statuts done are $total"
-
+}
 /*
 def todo(jsondata){
 def jsonString = jsondata

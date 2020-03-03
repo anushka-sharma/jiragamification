8dfd32d303296d16fc23a37578a7eaeb0d7e232c
Anushka Sharma <55789857+anushkadevops@users.noreply.github.com>
2020-03-03 12:29:21 +0530

    Update jiraCollector.groovy

--- vars/jiraCollector.groovy
+++ vars/jiraCollector.groovy
@@ -24,38 +24,70 @@ def jsonSlurper = new JsonSlurper()
 def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/outputInProgress.json"))
 def total = resultJson.total
 echo "Total no.of issues in $projectName with statuts in-progress are $total"
-}
 
 
-/*
-//def commiter=1
+	
+	
+/*	
 List<String> JSON = new ArrayList<String>();
 List<String> JCOPY = new ArrayList<String>();
-
-//Map<ImmutableList<String>, List<String>> map = new HashMap<ImmutableList<String>, List<String>>();
+List<String> JSON1= new ArrayList<String>();
 for(i=0;i<ecount;i++)
 {	 
   for(j=0;j<total;j++)
   {
+
  if(jsonObj.config.emails.email[i]==resultJson.values.author[j].emailAddress)
 	     {
 	JSON.add(resultJson.values[j])
 	//println(JSON) 
-              }
-  }
- } 
- 
-def count=JSON.size()
-println(jsonObj.config.emails.email[i])
-JCOPY[i]=(JsonOutput.toJson(JSON))
-map.put(ImmutableList.of(jsonObj.config.emails.email[i],count),JCOPY[i])
+     
+		     
+    }
+	
+      
+  
+      }
+	 
+	
+	 count=JSON.size()
+	 //  println(USER)
+         	
+ JSON1[i]=JSON.clone()
+	   JCOPY.add(["Email":jsonObj.config.emails.email[i],"Individual_commit":JSON1[i],"Commit_count":count])
+	
+	 
 	
 	
-JSON.clear()
+	 JSON.clear()
+	 
+
+	  
 }
-println(JCOPY)
+
+def jsonBuilder = new groovy.json.JsonBuilder()
+
+jsonBuilder.bitbucket(
+  "Total_commits": resultJson.values,
+ "Commit_count": resultJson.size(),
+ "Individual_commits":JCOPY
+)
+
+
+File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/commits1.json")
+file.write(jsonBuilder.toPrettyString())	
+/*def result = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/commits1.json"))
+def commits = result.bitbucket.Commit_count
+println(commits)*/
+}
+
+
 
 */
+
+
+
+
 
 
 

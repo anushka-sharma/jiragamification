3892f17d90d55dffd5c6f029763213defe6018f2
Anushka Sharma <55789857+anushkadevops@users.noreply.github.com>
2020-03-13 16:09:58 +0530

    Update jiraUserCollector.groovy

--- vars/jiraUserCollector.groovy
+++ vars/jiraUserCollector.groovy
@@ -23,9 +23,8 @@ withCredentials([usernamePassword(credentialsId: 'jira_password', passwordVariab
 sh """
      curl -X GET \
     -H -d -u $username:$password \
-     
      'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=assignee='${eMail}'%20AND%20(status%3D'\'"In%20Progress"\'')%20order%20by%20duedate&fields=id%2Ckey%2Cpriority' \
-  -H 'cache-control: no-cache' -o outputInProgressUser.json
+   -H 'cache-control: no-cache' -o outputInProgressUser.json
   """
   }   
 

41ceb8c0913e044cd9f9737005a61ab955c4ce24
Anushka Sharma <55789857+anushkadevops@users.noreply.github.com>
2020-03-13 16:11:11 +0530

    Update jiraUserCollector.groovy

--- vars/jiraUserCollector.groovy
+++ vars/jiraUserCollector.groovy
@@ -14,7 +14,7 @@ int ecount = jsonObj.config.emails.email.size()
 println("No of users "+ ecount)	
 //println(jsonObj.config)
 
-String a=jsonObj.config.emails.email
+String a=jsonObj.config.emails.email[i]
 String eMail=a.replaceAll("\\[", "").replaceAll("\\]","");
   
 env.name = eMail
@@ -23,7 +23,7 @@ withCredentials([usernamePassword(credentialsId: 'jira_password', passwordVariab
 sh """
      curl -X GET \
     -H -d -u $username:$password \
-     'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=assignee='${eMail}'%20AND%20(status%3D'\'"In%20Progress"\'')%20order%20by%20duedate&fields=id%2Ckey%2Cpriority' \
+     'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=assignee='${eMail[i]}'%20AND%20(status%3D'\'"In%20Progress"\'')%20order%20by%20duedate&fields=id%2Ckey%2Cpriority' \
    -H 'cache-control: no-cache' -o outputInProgressUser.json
   """
   }   
